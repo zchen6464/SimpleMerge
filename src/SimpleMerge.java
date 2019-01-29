04e799df34a4dd5317cc92f83635aab871082b9a
Z Chen <zchen6464@bths.edu>
2019-01-29 11:52:33 -0500

    Update SimpleMerge.java

--- src/SimpleMerge.java
+++ src/SimpleMerge.java
@@ -6,7 +6,23 @@
  */
 public class SimpleMerge {
     public static int[] simpleMerge(int[] arr1, int[] arr2) {
-
+        int[] out = new int[arr1.length+ arr2.length];
+        int selected = 0;
+        for(int i = 0; i < arr1.length;i++)
+        {
+            for(int j = i; j< arr2.length; j++)
+            {
+                if(arr2[j] < arr1[i])
+                {
+                    selected = arr2[j];
+                }
+                else
+                {
+                    selected = arr1[i];
+                }
+            }
+        }
+        return out;
     }
 
 }
